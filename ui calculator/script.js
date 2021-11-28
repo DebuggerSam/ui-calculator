f64047ccf5e8c6b9e6a8049b80bd11b6eb16d513
DebuggerSam <83894867+DebuggerSam@users.noreply.github.com>
2021-11-28 12:06:58 +0900

    This is simple layout for ui calculator
    Only layout

---
+++ ui calculator/script.js
@@ -0,0 +1,23 @@
+// const btn = document.querySelectorAll('.btn');
+
+// // btn.addEventListener('click', () => {
+// //   btn.classList.add('active');
+// // })
+
+
+// btn.forEach(item => {
+//   item.addEventListener('click', () => {
+//     item.classList.add('active');
+//   })
+// });
+
+
+// function rem(){
+//   btn.forEach(item => {
+//   item.addEventListener('click', () => {
+//     item.classList.remove('active');
+//   })
+//   });
+// }
+
+// setTimeout(rem, 1000);
